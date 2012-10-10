d9e8ff638814b0edee1e5083e58c023eb65d1c7e
Kun Zhao <zk.kyoto@gmail.com>
2012-10-10 14:31:08 +0900

    rename the folder

--- OceanVis/sh/run.sh
+++ OceanVis/sh/run.sh
@@ -7,7 +7,7 @@ do
     t=0
     while [ $t -ne 8 ]
     do
-./preProcess -s rm_rokka19_200803${d}__s_000$t.kvsml -outname 200803${d}_s_000$t.kvsml
+./Preprocess -s rm_rokka19_200803${d}__s_000$t.kvsml -outname 200803${d}_s_000$t.kvsml
     t=`expr $t + 1`
     done
 d=`expr $d + 1`
@@ -20,7 +20,7 @@ do
     t=0
     while [ $t -ne 8 ]
     do
-./compresion -f 200803${d}_s_000$t.kvsml -t 0315s.kvsml -sp 3 -PBVR -Bspline -b 2 -outpoint s_$s.kvsml
+./Compresion -f 200803${d}_s_000$t.kvsml -t 0315s.kvsml -sp 3 -PBVR -Bspline -b 2 -outpoint s_$s.kvsml
     t=`expr $t + 1`
     s=`expr $s + 1`
     done
