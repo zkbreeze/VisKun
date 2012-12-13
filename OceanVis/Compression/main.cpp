12e74cf1a45e8043e6ef3552f410ab0dac0e301e
Kun Zhao <zk.kyoto@gmail.com>
2012-12-13 16:15:49 +0900

    revise for tfunc

--- OceanVis/Compression/main.cpp
+++ OceanVis/Compression/main.cpp
@@ -117,16 +117,17 @@ int main( int argc, char** argv )
     param.exec();
     
     kvs::TransferFunction transferfunc = param.tfunc;
-    kvs::ColorMap cmap = transferfunc.colorMap();
-    cmap.addPoint( 0, kvs::RGBColor( 255, 255, 255) );
-    kvs::OpacityMap omap = transferfunc.opacityMap();
-    omap.addPoint( 0, 0.5 );
-    transferfunc.setColorMap( cmap );
-    transferfunc.setOpacityMap( omap );
     
     // load the original volume data
     kvs::StructuredVolumeObject* volume = new kvs::StructuredVolumeImporter( param.filename );
     transferfunc.setRange( volume->minValue(), volume->maxValue() );
+    kvs::ColorMap cmap = transferfunc.colorMap();
+    cmap.addPoint( volume->minValue(), kvs::RGBColor( 255, 255, 255) );
+    kvs::OpacityMap omap = transferfunc.opacityMap();
+    omap.addPoint( volume->minValue(), 0.5 );
+    transferfunc.setColorMap( cmap );
+    transferfunc.setOpacityMap( omap );
+
 
     std::string volumeName = param.outFilename;
         
